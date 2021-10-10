1991795e7f4bb442310ffd629f89aca4a3e1aba3
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-10-10 03:38:11 +0200

    Add selected block display.

--- Assets/Scripts/Controller/GameEditor/SelectedBlockTypeDisplay.cs
+++ Assets/Scripts/Controller/GameEditor/SelectedBlockTypeDisplay.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Sources;
 using Sources.Level;
 using UnityEngine;
@@ -6,13 +6,11 @@ using UnityEngine.UI;
 
 namespace Controller.GameEditor {
     [RequireComponent(typeof(Button))]
-    public class BlockTypeDisplay : MonoBehaviour {
+    public class SelectedBlockTypeDisplay : MonoBehaviour {
         private static readonly int ShaderTextureReference = Shader.PropertyToID("BlockTexture");
 
-        public Button button;
         public MeshFilter meshFilter;
         public MeshRenderer meshRenderer;
-        public bool showSelection = true;
 
         private BlockType _blockType;
 
@@ -26,24 +24,21 @@ namespace Controller.GameEditor {
         }
 
         private void Start() {
-            if (BlockType == null) {
-                Debug.Log("DISPLAY IS NULL!");
-                return;
-            }
-
-            button = GetComponent<Button>();
-
-            button.onClick.AddListener(() => {
-                EditorData.SelectedBlockType = BlockType;
-                EditorData.Metadata = new Dictionary<string, string>();
-            });
+            BlockType = EditorData.SelectedBlockType;
+            EditorData.OnSelectedBlockTypeChange += OnSelectedBlockChange;
         }
+
 
         private void Update() {
             meshRenderer.transform.rotation *= Quaternion.Euler(0, Time.deltaTime * 10, 0);
-            button.image.color = showSelection && BlockType == EditorData.SelectedBlockType
-                ? new Color(1, 0, 0, 0.5f)
-                : new Color(0, 0, 0, 0);
+        }
+
+        private void OnDestroy() {
+            EditorData.OnSelectedBlockTypeChange -= OnSelectedBlockChange;
+        }
+
+        private void OnSelectedBlockChange(BlockType type) {
+            BlockType = type;
         }
     }
 }
