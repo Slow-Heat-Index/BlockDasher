237f0da7b3d590f6f94bfcc2a68f44f72e91e322
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-10-27 11:36:19 +0200

    Fix intro.

--- Assets/IntroManager.cs
+++ Assets/IntroManager.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,26 +6,16 @@ using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class IntroManager : MonoBehaviour {
-    private VideoPlayer _videoPlayer;
-    [SerializeField] private Image _fadeToBlack;
-    [SerializeField] private VideoClip _videoClip;
-
-    private void Awake() {
-        _videoPlayer = GetComponent<VideoPlayer>();
-        _videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"SlowHeatIndex.mp4");
-    }
+    [SerializeField] private Image fade;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     // Start is called before the first frame update
     void Start() {
-        StartCoroutine(WaitVideoToFinish());
+        videoPlayer.loopPointReached += player => StartCoroutine(OnVideoFinish());
     }
 
-    IEnumerator WaitVideoToFinish() {
-        while (_videoPlayer.isPrepared) {
-            yield return null;
-        }
-        yield return new WaitForSeconds(6);
-        _fadeToBlack.DOFade(1, 1);
+    IEnumerator OnVideoFinish() {
+        fade.DOFade(1, 1);
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(1);
     }
--- Assets/Sources/LevelData.cs
+++ Assets/Sources/LevelData.cs
@@ -2,7 +2,7 @@
 
 namespace Sources {
     public static class LevelData {
-        public static LevelToLoad LevelToLoad; // = new LevelToLoad("Levels/lvl1", true);
+        public static LevelToLoad LevelToLoad = new LevelToLoad("Levels/IntroductoryLevel", true);
 
         public static void SetLevelToLoad(string path) {
             LevelToLoad = new LevelToLoad(path, true);
