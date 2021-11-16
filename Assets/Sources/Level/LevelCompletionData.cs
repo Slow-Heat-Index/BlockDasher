9ce5d74328b1e44ddf22df7a9c2aea5e3262b9fe
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-11-16 16:28:23 +0100

    Add LevelCompletionData.cs.

---
+++ Assets/Sources/Level/LevelCompletionData.cs
@@ -0,0 +1,9 @@
+﻿using Sources.Identification;
+
+namespace Sources.Level {
+    public struct LevelCompletionData {
+        public Identifier Level;
+        public int Steps;
+        public int Stars;
+    }
+}
