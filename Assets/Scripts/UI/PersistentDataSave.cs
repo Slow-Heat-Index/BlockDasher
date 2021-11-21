5c4e04fb40bffab8edcf4231c214c44587abd077
Gael Rial Costas <g.rial.2018@alumnos.urjc.es>
2021-11-21 22:07:51 +0100

    Add small script to save persistant data. Fix free coins button.

---
+++ Assets/Scripts/UI/PersistentDataSave.cs
@@ -0,0 +1,10 @@
+﻿using Data;
+using UnityEngine;
+
+namespace UI {
+    public class PersistentDataSave : MonoBehaviour {
+        public void Save() {
+            DataAccess.Save(PersistentDataContainer.PersistentData);
+        }
+    }
+}
